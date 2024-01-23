d5b038fc75fef36e7491b8e905f22ccc741b8770
omairazam <omairr.azam@gmail.com>
2024-01-24 03:46:02 +0500

    fix: update role authorization layer added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,6 +16,9 @@ import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserRoles } from 'src/shared/enums/user-roles.enum';
+import { AuthorizationGuard } from './authorization.guard';
 @Controller('auth')
 export class AuthController {
   private readonly logger = new Logger('AuthController');
@@ -41,6 +44,8 @@ export class AuthController {
 
   @UseGuards(JwtAuthGuard)
   @Patch('update_role')
+  @Roles(UserRoles.Admin)
+  @UseGuards(JwtAuthGuard, AuthorizationGuard)
   updateRole(@Body() updateRoleDto: UpdateRoleDto, @Request() req) {
     this.logger.log(`req ${req.user.id}`);
     return this.authService.updateRole(req.user.id, updateRoleDto);
